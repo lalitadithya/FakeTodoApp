48df0fff6b5795b5855b75971382eb70c8baf66d
lalitadithya1996@hotmail.com <lalitadithya1996@hotmail.com>
2020-05-04 18:24:06 +0530

    Added todo item view model

---
+++ FakeTodoApp/ViewModels/TodoItemViewModel.cs
@@ -0,0 +1,32 @@
+﻿using FakeTodoApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.I2c;
+
+namespace FakeTodoApp.ViewModels
+{
+    public class TodoItemViewModel : NotificationBase<TodoItem>
+    {
+        private TodoItem todoItem;
+
+        public TodoItemViewModel()
+        {
+            todoItem = new TodoItem();
+        }
+
+        public string Title
+        {
+            get
+            {
+                return This.Title;
+            }
+            set
+            {
+                SetProperty(This.Title, value, () => This.Title = value);
+            }
+        }
+    }
+}
